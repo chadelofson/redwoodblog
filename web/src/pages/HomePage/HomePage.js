d2da540a717fc4446c0d5c34214d92bb6ecc3601
chadelofson <chad.elofson@gmail.com>
2020-09-13 03:32:27 -0700

    Getting ready for deployment

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return <BlogLayout>
+    <BlogPostsCell />
+  </BlogLayout>
+}
+
+export default HomePage
